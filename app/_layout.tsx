3cd344e61c54c727aa0927d22fbfa582b4694cdc
Mücahit Özkanca <mmucahit17@hotmail.com>
2024-02-22 02:25:27 +0300

    initial and prebuild

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,17 +5,12 @@ import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 
-import { useColorScheme } from '@/components/useColorScheme';
 
 export {
   // Catch any errors thrown by the Layout component.
   ErrorBoundary,
 } from 'expo-router';
 
-export const unstable_settings = {
-  // Ensure that reloading on `/modal` keeps a back button present.
-  initialRouteName: '(tabs)',
-};
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
@@ -45,14 +40,10 @@ export default function RootLayout() {
 }
 
 function RootLayoutNav() {
-  const colorScheme = useColorScheme();
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
+        <Stack.Screen name="index" options={{ headerShown: true }} />
       </Stack>
-    </ThemeProvider>
   );
 }
---
+++ app/index.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+type Props = {}
+
+const Page = (props: Props) => {
+  return (
+    <View>
+      <Text>FIRST Page</Text>
+    </View>
+  )
+}
+
+export default Page
+
+const styles = StyleSheet.create({})
