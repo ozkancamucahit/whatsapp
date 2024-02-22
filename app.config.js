79aaaa2cef7d93c03e2d5d43e3445cbaf577492b
Mücahit Özkanca <mmucahit17@hotmail.com>
2024-02-22 23:32:54 +0300

    prebuild -fail

--- app.config.js
+++ app.config.js
@@ -1,7 +1,10 @@
 module.exports = {
-  name: 'MyApp',
+  name: 'WhatsAppCloni',
   version: '1.0.0',
   extra: {
     clerkPublishableKey: process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY,
   },
+  android: {
+    "package": "com.mucahit.whatsapp.cloni"
+  }
 };
