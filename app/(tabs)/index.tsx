92f033a61921e4c8e5e5f080381630067a0bd216
Mücahit Özkanca <mmucahit17@hotmail.com>
2024-02-22 20:57:50 +0300

    mask input and code field

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,17 @@
+
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+type Props = {}
+
+const Page = (props: Props) => {
+  return (
+    <View>
+      <Text>Page</Text>
+    </View>
+  )
+}
+
+export default Page
+
+const styles = StyleSheet.create({})
